252b7fc5305ecdd144fd85a0860090f12af0eb13
John Grey <johngrey4296@gmail.com>
2023-11-15 19:38:02 +0000

    [bump]: bibtex actions state retrieval

--- dootle/bibtex/actions.py
+++ dootle/bibtex/actions.py
@@ -38,6 +38,7 @@ from doot.actions.base_action import DootBaseAction
 from dootle.bibtex.writer import DootleBibtexWriter
 from dootle.latex.utils import latex
 from bibtexparser.bparser import BibTexParser
+from doot.utils.string_expand import expand_str, expand_key, expand_to_obj, expand_set
 
 printer = logmod.getLogger("doot._printer")
 
@@ -70,88 +71,77 @@ class _OverrideDict(dict):
 
 class BibtexInitAction(Action_p):
     """
-      Initialise a bibtex database. Override 'entry_transform' for customisation of loading.
+      Initialise a bibtex database. Override '_entry_transform' for customisation of loading.
 
-      pass a callable as the spec.args value tto use in stead of entry_transform
+      pass a callable as the spec.args value to use instead of _entry_transform
     """
-    outState = [DB_KEY]
 
     def __call__(self, spec, task_state:dict):
-        if DB_KEY in task_state:
+        data_key = expand_str(spec.kwargs.on_fail(DB_KEY).update_(), spec, task_state)
+        if data_key in task_state:
             return True
 
         db                   = b.bibdatabase.BibDatabase()
         db.strings           = _OverrideDict()
         printer.info("Bibtex Database Initialised")
 
-        return { DB_KEY : db }
+        return { data_key : db }
 
 class BibtexLoadAction(Action_p):
-    """ Parse all the bibtext files into a shared database.
+    """ Parse all the bibtext files into a task_state database, in place.
 
-      addFn to task_state[`entry_transform`] to use a custom entry transformer,
-      or subclass this and override self.entry_transform.
+      addFn to task_state[`_entry_transform`] to use a custom entry transformer,
+      or subclass this and override self._entry_transform.
 
       """
-    _toml_kwargs = [FILES_KEY, "ignore_nonstandard", "homogenise", "multi_parse", "crossref"]
-    inState      = [DB_KEY]
+    _toml_kwargs = ["update_", "transform", "current_year", "ignore_nonstandard", "homogenise", "multi_parse", "crossref"]
 
     def __call__(self, spec, task_state:dict):
-        try:
-            file_list            = task_state.get(FILES_KEY) or spec.kwargs.on_fail([], list)[FILES_KEY]()
-            if not isinstance(file_list, list):
-                file_list = [file_list]
+            year_key             = expand_str(spec.kwargs.on_fail(YEAR_KEY).year_(), spec, task_state)
+            db_key               = expand_str(spec.kwargs.on_fail(DB_KEY).update_(), spec, task_state, as_key=True)
+            from_key             = expand_str(spec.kwargs.on_fail("_from").from_(), spec, task_state, as_key=True)
+            transform_key        = expand_str(spec.kwargs.on_fail("transform").transform_(), spec, task_state, as_key=True)
 
-            if spec.kwargs.on_fail(None)[STATE_KEY]() in task_state:
-                file_list.append(task_state[spec.kwargs[STATE_KEY]])
+            file_list            = list(expand_set(from_key, spec, task_state, as_path=True))
+            try:
+                transform        = expand_to_obj(transform_key, spec, task_state)
+            except KeyError:
+                transform        = self._entry_transform
 
-            db                   = task_state[DB_KEY]
-            bparser              = self._make_parser(spec, db, task_state.get("entry_transform", None))
+            db                   = expand_to_obj(db_key, spec, task_state)
+            bparser              = self._make_parser(spec, db, transform)
 
-            printer.info("Attempting to load files: %s", file_list)
+            printer.info("Attempting to load files: %s", [str(x) for x in file_list])
             current_entry_count = len(db.entries)
-            for x in file_list:
-                loc = doot.locs[x]
+            for loc in file_list:
                 printer.info("Loading bibtex: %s", loc)
-                text = loc.read_bytes().decode("utf-8", "replace")
-                bparser.parse(text, partial=True)
-                printer.info("Loaded: %s entries",  len(db.entries) - current_entry_count)
-                current_entry_count = len(db.entries)
+                try:
+                    text = loc.read_bytes().decode("utf-8", "replace")
+                    bparser.parse(text, partial=True)
+                    printer.info("Loaded: %s entries",  len(db.entries) - current_entry_count)
+                    current_entry_count = len(db.entries)
+                except UnicodeDecodeError as err:
+                    printer.error("Unicode Error in File: %s, Start: %s", loc, err.start)
+                    return False
+                except Exception as err:
+                    printer.error("Bibtex File Loading Errored: %s : %s", loc, err)
+                    return False
 
 
             printer.info("Total DB Entry Count: %s", current_entry_count)
             if len(file_list) == 1:
                 loc = doot.locs[file_list[0]]
                 printer.info("Current year: %s", loc.stem)
-                return { YEAR_KEY : loc.stem }
+                return { year_key: loc.stem }
 
             return True
-        except UnicodeDecodeError as err:
-            printer.error("Unicode Error in File: %s, Start: %s", x, err.start)
-            return False
-        except Exception as err:
-            printer.error("Bibtex File Loading Errored: %s : %s", x, err)
-            return False
-
-
-    def entry_transform(self, entry) -> dict:
-        """
-          Transform for each bibtex entry on load
-          by default, ensures everything is unicode
-        """
-        for k,v in entry.items():
-            if 'url' in k or 'file' in k:
-                continue
-            entry[k] = NEWLINE_RE.sub(" ", latex.to_unicode(v))
-        entry['__as_unicode'] = True
-        return entry
 
     def _make_parser(self, spec, db, transform=None):
         """
           Builds a bibtexparser with default settings
         """
         bparser = BibTexParser(common_strings=False)
-        bparser.customization             = transform or self.entry_transform
+        bparser.customization             = transform or self._entry_transform
         bparser.ignore_nonstandard_types  = spec.kwargs.on_fail(False, bool).ignore_nonstandard()
         bparser.homogenise_fields         = spec.kwargs.on_fail(False, bool).homogenise()
         bparser.expect_multiple_parse     = spec.kwargs.on_fail(True, bool).multi_parse()
@@ -170,35 +160,60 @@ class BibtexLoadAction(Action_p):
         bparser.bib_database = db
         return bparser
 
+    def _entry_transform(self, entry) -> dict:
+        """
+          Transform for each bibtex entry on load
+          by default, ensures everything is unicode
+        """
+        for k,v in entry.items():
+            if 'url' in k or 'file' in k:
+                continue
+            entry[k] = NEWLINE_RE.sub(" ", latex.to_unicode(v))
+        entry['__as_unicode'] = True
+        return entry
 
 
 class BibtexMergeAction(Action_p):
 
-    shared_db = None
+    _toml_kwargs                                         = ["clear", "update_", "from_"]
+    shared_db : ClassVar[None|b.bibdatabase.BibDatabase] = None
 
     def __call__(self, spec, task_state):
         if BibtexMergeAction.shared_db is None or spec.kwargs.on_fail(False).clear():
-            BibtexMergeAction.shared_db = b.bibdatabase.BibDatabase()
+            printer.info("Initialising Shared DB")
+            BibtexMergeAction.shared_db         = b.bibdatabase.BibDatabase()
             BibtexMergeAction.shared_db.strings = _OverrideDict()
 
-        if DB_KEY in task_state:
-            BibtexMergeAction.shared_db.entries += task_state[DB_KEY].entries
+        db_key                                  = expand_str(spec.kwargs.on_fail(DB_KEY).from_(), spec, task_state, as_key=True)
+        data_key                                = expand_str(spec.kwargs.on_fail(DB_KEY).update_(), spec, task_state)
 
-        return {DB_KEY: BibtexMergeAction.shared_db}
+        try:
+            pre_count = len(BibtexMergeAction.shared_db.entries)
+            db = expand_to_obj(db_key, spec, task_state)
+            BibtexMergeAction.shared_db.entries += db.entries[:]
+            post_count = len(BibtexMergeAction.shared_db.entries)
+        except KeyError:
+            printer.debug("No Database found at: %s", db_key)
+
+
+        printer.info("Updated Entry Count: %s", len(BibtexMergeAction.shared_db.entries))
+        return { data_key : BibtexMergeAction.shared_db}
 
 
 class BibtexToStrAction(Action_p):
     """
       Convert a bib database to a string for writing to a file.
     """
-    inState  = [DB_KEY]
-    outState = [TEXT_KEY]
+    _toml_kwargs = ["from_", "update_"]
+
 
     def __call__(self, spec, task_state):
-        db     = task_state[DB_KEY]
-        writer = DootleBibtexWriter()
-        result = writer.write(db)
-        return { TEXT_KEY : result }
+        db_key                                  = expand_str(spec.kwargs.on_fail(DB_KEY).from_(), spec, task_state, as_key=True)
+        data_key                                = expand_str(spec.kwargs.on_fail(TEXT_KEY).update_(), spec, task_state)
+        db                                      = expand_to_obj(db_key, spec, task_state)
+        writer                                  = DootleBibtexWriter()
+        result                                  = writer.write(db)
+        return { data_key : result }
 
 class BibtexEntryTransformer(Action_p):
     """
@@ -216,30 +231,36 @@ class BibtexEntryTransformer(Action_p):
       pass a callable in the spec to use that instead
       The callable modifies the entry *in place*
     """
-    inState = [DB_KEY]
+    _toml_kwargs = ["from_"]
 
     def __call__(self, spec, task_state):
-        db       = task_state[DB_KEY]
+        db_key                                  = expand_str(spec.kwargs.on_fail(DB_KEY).from_(), spec, task_state)
+        db       = task_state[db_key]
         match spec.args:
             case []:
-                transforms = [self.entry_transform]
+                transforms = [self._entry_transform]
             case [*keys]:
                 transforms = [task_state[key] for key in keys]
 
         assert(all(callable(x) for x in transforms))
         printer.debug("Transforming Entries with: %s", spec.args or "default")
-        to_remove = []
+        to_remove = set()
         for entry in db.entries:
             for transform in transforms:
                 if not transform(entry, spec, task_state):
-                    to_remove_.append(entry)
+                    to_remove.add(entry)
                     break
 
+        curr_count = len(db.entries)
         map(db.entries.remove, to_remove)
+        reduced_count = len(db.entries)
+
+        if reduced_count + len(to_remove) != curr_count:
+            raise doot.errors.DootActionError("Amounts don't match for removing entries")
 
         return True
 
-    def entry_transform(self, entry, spec, task_state) -> None:
+    def _entry_transform(self, entry, spec, task_state) -> None:
         """ convert processed __{field}'s into strings in {field},
         removing the the __{field} once processed
         """
