009b0d2bdba432e51581a7ff86ed065fbfcae0c1
John Grey <johngrey4296@gmail.com>
2024-01-14 14:03:22 +0000

    [refactor]: spider runner

--- dootle/spiders/runner.py
+++ dootle/spiders/runner.py
@@ -68,6 +68,7 @@ from doot._abstract import Job_i, Task_i, FailPolicy_p
 from doot._abstract import TaskTracker_i, TaskRunner_i, TaskBase_i, ReportLine_i, Action_p
 from doot.utils.signal_handler import SignalHandler
 from doot.structs import DootTaskSpec, DootActionSpec
+from doot.control.base_runner import BaseRunner, logctx
 
 from zope.interface import implementer
 from twisted.internet import reactor, protocol, defer, threads
@@ -86,33 +87,25 @@ execute_level : Final[str] = doot.constants.DEFAULT_EXECUTE_LEVEL
 reactor_timeout = doot.config.on_fail(2, int).settings.tasks.reactor_timeout()
 
 @doot.check_protocol
-class DootleReactorRunner(TaskRunner_i):
+class DootleReactorRunner(BaseRunner, TaskRunner_i):
     """ The simplest reactor task runner
       https://stackoverflow.com/questions/8532131
+
+     wraps steps of running jobs/tasks/actions into deferreds
     """
 
     def __init__(self:Self, *, tracker:TaskTracker_i, reporter:Reporter_i, policy=None):
         super().__init__(tracker=tracker, reporter=reporter, policy=policy)
-        self._printer_level_stack = []
-        self.step                 = 0
-        self.default_SLEEP_LENGTH = doot.config.on_fail(0.2, int|float).settings.general.task.sleep()
         scrapy_settings           = dict(doot.config.scrapy.settings.items())
         self.crawler              = CrawlerRunner(settings=scrapy_settings)
         self.reactor              = reactor
-
-    def __enter__(self) -> Any:
-        printer.info("---------- Reactor Starting ----------", extra={"colour" : "green"})
-        return
-
-    def __exit__(self, exc_type, exc_value, exc_traceback) -> bool:
-        self._set_print_level("INFO")
-        printer.info("")
-        printer.info("---------- Reactor Finished ----------", extra={"colour":"green"})
-        self._finish()
-        return
+        self._enter_msg = "---------- Reactor Starting ----------"
+        self._exit_msg  = "---------- Reactor Finished ----------"
 
     def __call__(self, *tasks:str):
-        reactor.callWhenRunning(self._run_next_task, *tasks)
+        """ Queues _run_next_task and starts the reactor """
+        self.tracker.queue_task(*tasks)
+        reactor.callWhenRunning(self._run_next_task)
         reactor.run()
 
     def _run_next_task(self):
@@ -125,29 +118,22 @@ class DootleReactorRunner(TaskRunner_i):
         d.addCallback(self._schedule_next)
         d.unpause()
 
-
     def _handle_task(self, task) -> defer.Deferred[None|Task]:
-        self._set_print_level("INFO")
-        if task is None:
-            logging.debug("Task is None")
-            return None
-
-        self.step += 1
+        printer.setLevel("INFO")
         match task:
+            case None:
+                logging.debug("Task is None")
+                return None
             case Job_i():
+                self.step += 1
                 return defer.maybeDeferred(self._expand_job, task)
             case Task_i():
+                self.step += 1
                 return defer.maybeDeferred(self._execute_task, task)
             case _:
                 raise doot.errors.DootTaskError("Unknown Task Base: %s", task, task=task)
 
-    def _handle_task_success(self, task) -> defer.Deferred[None|Task]:
-        if task:
-            self.tracker.update_state(task, self.tracker.state_e.SUCCESS)
-        return task
-
     def _handle_failure(self, failure) -> defer.Failure:
-        # Handle problems:
         match failure:
             case doot.errors.DootTaskInterrupt():
                 breakpoint()
@@ -169,107 +155,111 @@ class DootleReactorRunner(TaskRunner_i):
                 return failure
 
     def _schedule_next(self, task) -> defer.Deferred:
-        if task is not None:
-            self._set_print_level(task.spec.print_levels.on_fail(sleep_level).sleep())
-            sleep_len = task.spec.extra.on_fail(self.default_SLEEP_LENGTH, int|float).sleep()
-            printer.info("[Sleeping (%s)...]", sleep_len, extra={"colour":"white"})
-            return self.reactor.callLater(sleep_len, self._run_next_task)
-        elif not bool(self.tracker.task_queue):
+        """ Schedules the next available task """
+        if task is None and not bool(self.tracker.task_queue):
             printer.info("Scheduling Reactor shutdown")
             return self.reactor.callLater(reactor_timeout, self.reactor.stop)
-        else:
+
+        if task is None:
             printer.info("Task is none, tracker is: %s", bool(self.tracker.task_queue))
             return self.reactor.callLater(reactor_timeout, self.reactor.stop)
 
+        with logctx(task.spec.print_levels.on_fail(sleep_level).sleep()):
+            sleep_len = task.spec.extra.on_fail(self.default_SLEEP_LENGTH, int|float).sleep()
+            printer.info("[Sleeping (%s)...]", sleep_len, extra={"colour":"white"})
+            return self.reactor.callLater(sleep_len, self._run_next_task)
 
 
-
-    def _expand_job(self, job:Job_i):
+    def _expand_job(self, job:Job_i) -> Job_i:
         """ turn a job into all of its tasks, including teardowns """
         logmod.debug("-- Expanding Job %s: %s", self.step, job.name)
-        self._set_print_level(job.spec.print_levels.on_fail(head_level).head())
-        printer.info("---- Job %s: %s", self.step, job.name, extra={"colour":"magenta"})
-        if bool(job.spec.actions):
-            printer.warning("-- Job %s: Actions were found in job spec, but jobs don't _run_next_task actions")
-        count = 0
-        self._set_print_level(job.spec.print_levels.on_fail(build_level).build())
-        for task in job.build():
-            match task:
-                case Job_i():
-                    printer.warning("Jobs probably shouldn't build jobs: %s : %s", job.name, task.name)
-                    self.tracker.add_task(task, no_root_connection=True)
-                    self.tracker.queue_task(task.name)
-                case Task_i():
-                    self.tracker.add_task(task, no_root_connection=True)
-                    self.tracker.queue_task(task.name)
-                case DootTaskSpec():
-                    self.tracker.add_task(task, no_root_connection=True)
-                    self.tracker.queue_task(task.name)
-                case _:
-                    raise doot.errors.DootTaskError("Job %s Built a Bad Value: %s", job.name, task, task=job.spec)
+        with logctx(job.spec.print_levels.on_fail(head_level).head()) as p:
+            p.info("---- Job %s: %s", self.step, job.name, extra={"colour":"magenta"})
+            if bool(job.spec.actions):
+                p.warning("-- Job %s: Actions were found in job spec, but jobs don't _run_next_task actions")
 
-            count += 1
+        with logctx(job.spec.print_levels.on_fail(build_level).build()):
+            count = 0
+            for task in job.build():
+                match task:
+                    case Job_i():
+                        printer.warning("Jobs probably shouldn't build jobs: %s : %s", str(job.name), str(task.name))
+                        self.tracker.add_task(task, no_root_connection=True)
+                        self.tracker.queue_task(task.name)
+                    case Task_i():
+                        self.tracker.add_task(task, no_root_connection=True)
+                        self.tracker.queue_task(task.name)
+                    case DootTaskSpec():
+                        self.tracker.add_task(task, no_root_connection=True)
+                        self.tracker.queue_task(task.name)
+                    case _:
+                        raise doot.errors.DootTaskError("Job %s Built a Bad Value: %s", str(job.name), task, task=str(job.name))
+
+                count += 1
 
         logmod.debug("-- Job %s Expansion produced: %s tasks", job.name, count)
         return job
 
     def _execute_task(self, task:Task_i) -> defer.Deferred[Task]:
         """ execute a single task's actions """
-        logmod.debug("---- Executing Task %s: %s", self.step, task.name)
-        self._set_print_level(task.spec.print_levels.on_fail(head_level).head())
-        printer.info("---- Task %s: %s", self.step, task.name, extra={"colour":"magenta"})
+        with logctx(task.spec.print_levels.on_fail(head_level).head()) as p:
+            p.info("---- Task %s: %s", self.step, task.name, extra={"colour":"magenta"})
 
         d = defer.succeed(task)
         d.pause()
         action_count = 0
         action_result = ActRE.SUCCESS
-        self._set_print_level(task.spec.print_levels.on_fail(build_level).build())
-        for action in task.actions:
-            match action:
-                case _ if dry_run:
-                    logging.info("Dry Run: Not executing action: %s : %s", task.name, action, extra={"colour":"cyan"})
-                case DootActionSpec() if action.fun is None:
-                    raise doot.errors.DootTaskError("Task %s Failed: Produced an action with no callable: %s", task.name, action, task=task.spec)
-                case DootActionSpec():
-                    match self._execute_action(action_count, action, task):
-                        case ActRE.SKIP:
-                            action_result = ActRE.SKIP
-                        case defer.Deferred() as x:
-                            d.addCallback(lambda _: x)
-                        case _:
-                            pass
-                case _:
-                    raise doot.errors.DootTaskError("Task %s Failed: Produced a bad action: %s", task.name, action, task=task.spec)
+        with logctx(task.spec.print_levels.on_fail(build_level).build()) as p:
+            for action in task.actions:
+                match action:
+                    case DootActionSpec() if action.fun is None:
+                        raise doot.errors.DootTaskError("Task %s Failed: Produced an action with no callable: %s", task.name, action, task=task.spec)
+                    case DootActionSpec():
+                        match self._execute_action(action_count, action, task):
+                            case ActRE.SKIP:
+                                action_result = ActRE.SKIP
+                            case defer.Deferred() as x:
+                                d.addCallback(lambda _: x)
+                            case _:
+                                pass
+                    case _:
+                        raise doot.errors.DootTaskError("Task %s Failed: Produced a bad action: %s", task.name, action, task=task.spec)
 
-            self._set_print_level(task.spec.print_levels.on_fail(build_level).build())
-            action_count += 1
-            if action_result is ActRE.SKIP:
-                printer.info("------ Remaining Task Actions skipped by Action Instruction")
-                break
-        else: # Only try to add more tasks if the actions completed successfully, and weren't skipped
-            for new_task in task.maybe_more_tasks():
-                printer.warning("Skipping task sub: %s", new_task)
+                action_count += 1
+                if action_result is ActRE.SKIP:
+                    p.info("------ Remaining Task Actions skipped by Action Instruction")
+                    break
 
-        printer.debug("------ Task Executed %s Actions", action_count)
-        d.addCallback(lambda _: task)
-        d.unpause()
+            else: # Only try to add more tasks if the actions completed successfully, and weren't skipped
+                for new_task in task.maybe_more_tasks():
+                    p.warning("Skipping task sub: %s", new_task)
+
+            p.debug("------ Task Executed %s Actions", action_count)
+            d.addCallback(lambda _: task)
+            d.unpause()
         return d
 
     def _execute_action(self, count, action, task) -> ActRE|defer.Deferred:
         """ Run the given action of a specific task  """
-        logmod.debug("------ Executing Action %s: %s for %s", count, action, task.name)
-        self._set_print_level(task.spec.print_levels.on_fail(action_level).action())
-        task.state['_action_step'] = count
-        printer.info("------ Action %s.%s: %s", self.step, count, action.do, extra={"colour":"cyan"})
-        printer.debug("------ Action %s.%s: args=%s kwargs=%s. state keys = %s", self.step, count, action.args, dict(action.kwargs), list(task.state.keys()))
-        action.verify(task.state)
-        task.state['_reactor'] = self.reactor
-        task.state['_crawler'] = self.crawler
+        if dry_run:
+            logging.info("Dry Run: Not executing action: %s : %s", task.name, action, extra={"colour":"cyan"})
+            return ActRE.SUCCESS
 
-        self._set_print_level(task.spec.print_levels.on_fail(execute_level).execute())
-        result = action(task.state)
-        self._set_print_level(task.spec.print_levels.on_fail(action_level).action())
-        printer.debug("-- Action Result: %s", result)
+        logmod.debug("------ Executing Action %s: %s for %s", count, action, task.name)
+        result = None
+        with logctx(task.spec.print_levels.on_fail(action_level).action()) as p:
+            task.state['_action_step'] = count
+            p.info("------ Action %s.%s: %s", self.step, count, action.do, extra={"colour":"cyan"})
+            p.debug("------ Action %s.%s: args=%s kwargs=%s. state keys = %s", self.step, count, action.args, dict(action.kwargs), list(task.state.keys()))
+            action.verify(task.state)
+            task.state['_reactor'] = self.reactor
+            task.state['_crawler'] = self.crawler
+
+            with logctx(task.spec.print_levels.on_fail(execute_level).execute()):
+                result = action(task.state)
+
+            p.debug("-- Action Result: %s", result)
+
         match result:
             case defer.Deferred():
                 pass
@@ -289,16 +279,3 @@ class DootleReactorRunner(TaskRunner_i):
 
         logmod.debug("------ Action Execution Complete: %s for %s", action, task.name)
         return result
-
-    def _finish(self):
-        """finish running tasks, summarizing results"""
-        logging.info("Task Running Completed")
-        printer.info("Final Summary: ")
-
-    def _set_print_level(self, level=None):
-        """
-        Utility to set the print level, or reset it if no level is specified.
-        the Step Runner subclass overrides this to allow interactive control of the print level
-        """
-        if level:
-            printer.setLevel(level)
