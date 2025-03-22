81f7e4c4ecdb427a0f27d84b2edcdd15bd3643e0
John Grey <johngrey4296@gmail.com>
2025-03-22 11:26:32 +0000

    [initial]: reporter v2 interface

---
+++ dootle/reporters/_interface.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+"""
+
+"""
+# ruff: noqa:
+
+# Imports:
+from __future__ import annotations
+
+# ##-- stdlib imports
+import datetime
+import enum
+import functools as ftz
+import itertools as itz
+import logging as logmod
+import pathlib as pl
+import re
+import time
+import types
+import collections
+import contextlib
+import hashlib
+from copy import deepcopy
+from uuid import UUID, uuid1
+from weakref import ref
+import atexit # for @atexit.register
+import faulthandler
+# ##-- end stdlib imports
+
+# ##-- types
+# isort: off
+import abc
+import collections.abc
+from typing import TYPE_CHECKING, cast, assert_type, assert_never
+from typing import Generic, NewType, Never
+# Protocols:
+from typing import Protocol, runtime_checkable
+# Typing Decorators:
+from typing import no_type_check, final, override, overload
+
+if TYPE_CHECKING:
+    from jgdv import Maybe
+    from typing import Final
+    from typing import ClassVar, Any, LiteralString
+    from typing import Self, Literal
+    from typing import TypeGuard
+    from collections.abc import Iterable, Iterator, Callable, Generator
+    from collections.abc import Sequence, Mapping, MutableMapping, Hashable
+
+##--|
+
+# isort: on
+# ##-- end types
+
+##-- logging
+logging = logmod.getLogger(__name__)
+##-- end logging
+
+# Vars:
+TRACE_LINES           : Final[dict[str, str|tuple[str,str,str]]] = {
+    "root"            : "┳",
+    "wait"            : "┃",
+    "act"             : "┼◇",
+    "branch"          : ("┣", "─", "─╮"),
+    "resume"          : ("┣", "╌", "╌╮"),
+    "inactive"        : "┊",
+    "begin"           : "▼",
+    "join"            :  ("┢◀", "─", "─╯"),
+    "pause"           :  ("┝", "╌", "╌╯"),
+    "final"           : "┻",
+    "fail"            : "❌",
+    "gap"             : " ",
+}
+
+BRANCH_PARTS : Final[dict[str, str]] = {
+    "branch-extend"   : "─",
+    "branch-start"    : "╮",
+    "branch-activate" : "▼",
+    "branch-return"   : "╯",
+    "branch-reduce"   : "─",
+    "branch-join"     : "◀",
+}
+
+TRACE_LINES_ASCII     : Final[dict[str, str]] = {
+    "root"            : "Y",
+    "wait"            : "|",
+    "act"             : "| ->",
+    "branch"          : "|->=[",
+    "inactive"        : ":",
+    "begin"           :     "Y",
+    "return"          :   "=<]",
+    "pause"           :   "-<]",
+    "return-terminal" : "|"
+    "resume"          : "|->-[",
+    "join"            : "|<<<",
+    "final"           : "o",
+    "fail"            : "X",
+    "gap"             : " ",
+}
+
+# eg: "{┣─}{╮}"
+LINE_PASS_FMT : Final[str] = "{ctx}{act}"
+# eg: "{┊ ┊ }{┃} [{blah}] : {bloo}"
+LINE_MSG_FMT  : Final[str] = "{ctx}{act} [{info}] : {msg}"
+# Body:
+
+class AltReporter_p(Protocol):
+    """
+    A Re-entrant ctx manager, used for reporting user-level information about a
+    task workflow run.
+
+    """
+    level    : int
+    ctx      : list[str]
+    trace    : list
+    line_fmt : str
+    msg_fmt  : str
+
+    def __enter__(self) -> Self:
+        # calls branch|resume
+        # level+
+        pass
+
+    def __exit(self, *exc:Any) -> bool:
+        # pause|join|fail|return
+        # level-
+        pass
+
+    def root(self) -> bool:
+        # pass fmt
+        pass
+
+    def wait(self) -> None:
+        # pass fmt
+        pass
+
+    def act(self, info:str, msg:str) -> None:
+        # msg fmt
+        pass
+
+    def fail(self, info:str, msg:str) -> None:
+        # msg fmt
+        pass
+
+    def branch(self, name:str) -> None:
+        # pass fmt
+        pass
+
+    def pause (self, reason:str) -> None:
+        # msg fmt
+        pass
+
+    def join(self, state:list[str]) -> None:
+        # Maybe msg fmt
+        pass
+
+    def resume(self, name:str) -> None:
+        # msg fmt
+        pass
+
+    def final(self) -> bool:
+        # pass fmt
+        pass
+
+    def summary(self) -> bool:
+        pass
