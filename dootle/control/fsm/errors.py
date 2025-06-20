acac86f2c07e4af05896e8e3f0724e94e72ffa35
John Grey <johngrey4296@gmail.com>
2025-06-20 23:03:43 +0100

    [add]: errors for control.fsm

---
+++ dootle/control/fsm/errors.py
@@ -0,0 +1,21 @@
+"""
+
+
+"""
+from __future__ import annotations
+
+# Import:
+from typing import Final
+from jgdv.debugging import TraceBuilder
+from doot.errors import DootError
+
+##--| Error Messages
+
+
+##--| Error Classes
+
+class FSMSkip(DootError):
+    pass
+
+class FSMHalt(DootError):
+    pass
